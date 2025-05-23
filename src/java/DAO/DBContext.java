96acdc61ededfc0d18fecd273f8c8fd57d1f254b
Hoang Tu Uyen (K18 HL) <uyenhthe186216@fpt.edu.vn>
2025-05-23 09:45:31 +0700

    Initial commit

---
+++ src/java/DAO/DBContext.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBContext {
+    private static DBContext instance = new DBContext();
+    Connection connection;
+    
+    public static DBContext getInstance(){
+        return instance;
+    }
+    
+    public Connection getConnection(){
+        return connection;
+    }
+    
+    private DBContext(){
+        try{
+            if(connection == null || connection.isClosed()){
+		String user = "sa";
+		String password = "sa";
+		String url = "jdbc:sqlserver://MSI:1433;databaseName=A;TrustServerCertificate=true;";
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		connection = DriverManager.getConnection(url, user,password);
+	    }
+            
+        }catch(Exception e){
+            connection = null;
+        }
+    }
+}
